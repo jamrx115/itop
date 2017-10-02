c010844a901eecbbec50e3e292dccf1f0fbf7de5
root <root@ip-172-31-21-145.us-east-2.compute.internal>
2017-10-02 00:43:29 +0000

    Version salida a produccion

--- conf/production/config-itop.php
+++ conf/production/config-itop.php
@@ -29,7 +29,7 @@ $MySettings = array(
 
 	// app_root_url: Root URL used for navigating within the application, or from an email to the application (you can put $SERVER_NAME$ as a placeholder for the server's name)
 	//	default: ''
-	'app_root_url' => 'http://127.0.0.1:8081/itop/',
+	'app_root_url' => 'http://13.58.166.173/itop/',
 
 	// buttons_position: Position of the forms buttons: bottom | top | both
 	//	default: 'both'
@@ -80,7 +80,7 @@ $MySettings = array(
 
 	'db_name' => 'itop_alltic',
 
-	'db_pwd' => '',
+	'db_pwd' => 'root',
 
 	'db_subname' => '',
 
@@ -237,7 +237,8 @@ $MyModuleSettings = array(
 		'debug' => false,
 	),
 	'combodo-sla-computation' => array (
-		'coverage_oql' => 'SELECT CoverageWindow AS cw JOIN lnkCustomerContractToService AS l1 ON l1.coveragewindow_id = cw.id JOIN CustomerContract AS cc ON l1.customercontract_id = cc.id WHERE cc.org_id= :this->org_id AND l1.service_id = :this->service_id',
+	/*	'coverage_oql' => 'SELECT CoverageWindow',*/
+                'coverage_oql' => 'SELECT CoverageWindow AS cw JOIN lnkCustomerContractToService AS l1 ON l1.coveragewindow_id = cw.id JOIN CustomerContract AS cc ON l1.customercontract_id = cc.id WHERE cc.org_id= :this->org_id AND l1.service_id = :this->service_id',
 		'holidays_oql' => 'SELECT Holiday',
 		'deadline_format' => '$date$ ($difference$)',
 	),
